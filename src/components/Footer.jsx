9993a1532fe0b9a88036b153b40b47bc6f19b320
shravani-d-s <mailtoshrau@gmail.com>
2025-11-17 10:13:52 +0530

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import '../index.css';
 import '../App.css';
 
-<Footer />
-
 function Footer() {
     return (
-        <footer className="bg-blue-100 text-center text-sm text-gray-600 py-4 mt-8 border-t border-blue-200">
+        <footer className="w-screen relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] bg-blue-100 text-center text-sm text-gray-600 py-4 mt-8 border-t border-blue-200">
             &copy; {new Date().getFullYear()} VNIT Gatepass Management System. &copy; All rights reserved.
         </footer>
+
     );
 }
+
 export default Footer;
